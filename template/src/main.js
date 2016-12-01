4de3fb1f8301234ac332fef0c3b620d62ab99ded
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-12-01 01:59:16 -0800

    Toggle Vuex store inclusion in view model

--- template/src/main.js
+++ template/src/main.js
@@ -9,6 +9,9 @@ import locale from 'element-ui/lib/locale/lang/en'{{#if_eq lintConfig "airbnb"}}
 import '../theme/index.css'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/element}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{#vuex}}
+import store from './store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vuex}}
 
 {{#element}}
 Vue.use(ElementUI, { locale }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
@@ -17,6 +20,9 @@ Vue.use(ElementUI, { locale }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  {{#vuex}}
+  store,
+  {{/vuex}}
   {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}

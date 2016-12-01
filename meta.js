24420b6767a943fadf322e31ac857d7c22128986
Jan Van Bruggen <jancvanbruggen@gmail.com>
2016-12-01 01:35:44 -0800

    Add CLI confirmation to use Vuex

--- meta.js
+++ meta.js
@@ -85,6 +85,10 @@ module.exports = {
     "sass": {
       "type": "confirm",
       "message": "Enable Sass to pre-process Vue component styles?"
+    },
+    "vuex": {
+      "type": "confirm",
+      "message": "Use Vuex to manage application state?"
     }
   },
   "filters": {
